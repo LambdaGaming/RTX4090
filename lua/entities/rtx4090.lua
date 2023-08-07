6738ac7e663cb64cfc8a4af822eae2e207221359
LambdaGaming <starwars18m@gmail.com>
2023-08-07 01:40:32 -0400

    Sound change

--- lua/entities/rtx4090.lua
+++ lua/entities/rtx4090.lua
@@ -8,16 +8,6 @@ ENT.Spawnable = true
 ENT.AdminOnly = true
 ENT.Category = "Fun + Games"
 
-function ENT:SpawnFunction( ply, tr, name )
-	if !tr.Hit then return end
-	local SpawnPos = tr.HitPos + tr.HitNormal
-	local ent = ents.Create( name )
-	ent:SetPos( SpawnPos )
-	ent:Spawn()
-	ent:Activate()
-	return ent
-end
-
 function ENT:Initialize()
     self:SetModel( "models/rtx4090.mdl" )
 	self:SetMoveType( MOVETYPE_VPHYSICS )
@@ -31,14 +21,13 @@ function ENT:Initialize()
 	if phys:IsValid() then
 		phys:Wake()
 	end
-	self.Snd = CreateSound( self, "ambient/levels/labs/teleport_mechanism_windup2.wav" )
 	self.Active = false
 end
 
 function ENT:Use( ply )
 	if self.Active then return end
 	self:Ignite( 10 )
-	self.Snd:PlayEx( 0.5, 150 )
+	self:EmitSound( "gpufan.mp3" )
 	self.Active = true
 	timer.Simple( 10, function()
 		if IsValid( self ) then
@@ -50,7 +39,6 @@ function ENT:Use( ply )
 			local ed = EffectData()
 			ed:SetOrigin( self:GetPos() )
 			util.Effect( "cball_explode", ed )
-			self.Snd:Stop()
 			self:Remove()
 		end
 	end )
